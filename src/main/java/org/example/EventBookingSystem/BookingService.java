4795497576b4078d06426c730299c455a1f24772
aidandegiorgio <aidan.degiorgio.i79876@mcast.edu.mt>
2026-05-03 21:18:10 +0200

    Event class

--- src/main/java/org/example/EventBookingSystem/BookingService.java
+++ src/main/java/org/example/EventBookingSystem/BookingService.java
@@ -1,4 +1,5 @@
 package org.example.EventBookingSystem;
 
 public class BookingService {
+    //empty
 }
--- src/main/java/org/example/EventBookingSystem/Event.java
+++ src/main/java/org/example/EventBookingSystem/Event.java
@@ -1,4 +1,48 @@
 package org.example.EventBookingSystem;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
 
-public class Event {
+class Event {
+
+    private int id;
+    private String name;
+    public ChronoLocalDate date;
+    private int totalSeats;
+    public int availableSeats;
+
+    public Event(int id, String name, ChronoLocalDate date, int totalSeats) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.totalSeats = totalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public boolean isSoldOut() {
+        if (availableSeats <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isInPast() {
+
+        LocalDate currentDate = LocalDate.now();
+
+        if (currentDate.isAfter(date)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean hasCapacityFor(int people) {
+        if ((availableSeats - people) >= 0) {
+            return true;
+        }
+        return false;
+    }
 }
